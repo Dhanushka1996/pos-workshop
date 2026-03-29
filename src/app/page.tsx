3e3437e0d4374828fa9dfe41aa3f19d6660b7f49
Dhanushka1996 <dhanukoggala@gmail.com>
2026-03-29 15:30:03 +0530

    fix missing prisma import

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { getServerSession } from 'next-auth';
 import { authOptions } from '@/lib/auth';
 export const runtime = "nodejs";
 export const dynamic = 'force-dynamic';
-
+console.log("SECRET:", process.env.af1307a4a872d9276a29e50be24764df);
 export default async function RootPage() {
   const session = await getServerSession(authOptions);
   redirect(session ? '/dashboard' : '/login');
